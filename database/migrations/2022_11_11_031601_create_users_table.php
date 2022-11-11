9dd6886498910aafe8aa1d4b6ab3067c470670c5
MikhailShapshay <skiv80@yandex.kz>
2022-11-11 10:30:29 +0600

    Users: model, factory, seed

---
+++ database/migrations/2022_11_11_031601_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->bigInteger('whom_to_give')->nullable(); // кому дарит
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('users')) {
+            Schema::dropIfExists('users');
+        }
+    }
+}
